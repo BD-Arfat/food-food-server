54fb6b3bfc5fa8cfb4229fefb7e3b13574e01331
BD-Arfat <arfat1islam5162@gmail.com>
2023-07-01 22:47:47 +0600

    add

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ require('dotenv').config()
 //////////////////////
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p7jtpy2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,7 +30,8 @@ async function run() {
   try {
     const productCollection = client.db('food').collection('products');
     const dronkCollection = client.db('food').collection('dronk-product');
-    const sponsorCollection = client.db('food').collection('sponsor ');
+    const sponsorCollection = client.db('food').collection('sponsor');
+    const reviewsCollection = client.db('food').collection('reviews');
 
 
     app.get('/product', async (req, res) => {
@@ -45,6 +46,59 @@ async function run() {
         const products = await cursor.toArray();
         res.send(products)
       });
+      app.get('/products/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const service = await productCollection.findOne(query);
+        res.send(service)
+      });
+
+
+      // reviews 
+
+      app.post('/reviews', async(req, res)=>{
+        const user = req.body;
+        const result = await reviewsCollection.insertOne(user);
+        res.send(result)
+      });
+
+      app.get('/reviews', async(req, res)=>{
+        const query = {};
+        const cursor = await reviewsCollection.find(query).toArray();
+        res.send(cursor);
+      });
+
+      // app.get('/reviews/:id', async(req, res)=>{
+      //   const productId = req.params.id;
+      //   const query = {productId : productId};
+      //   const result = await reviewsCollection.findOne(query);
+      //   res.send(result)
+      // })
+
+      app.get('/reviews/:id',async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await reviewsCollection.findOne(query);
+        res.send(result)
+      })
+
+      // end reviews 
+      // my reviews
+      app.get('/reviews', async(req, res)=>{
+        const email = req.params.email;
+        const query = {email : email};
+        const result = await reviewsCollection.find(query).toArray();
+        res.send(result)
+      })
+
+      app.delete('/reviews/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await reviewsCollection.deleteOne(query);
+        res.send(result)
+      })
+
+      // end my reviews
 
 
       app.get('/drink', async(req, res)=>{
@@ -52,12 +106,25 @@ async function run() {
         const result = await dronkCollection.find(query).limit(6).toArray();
         res.send(result)
       });
+      app.get('/drinks', async(req, res)=>{
+        const query = {};
+        const result = await dronkCollection.find(query).toArray();
+        res.send(result)
+      })
+      app.get('/drinks/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const service = await dronkCollection.findOne(query);
+        res.send(service)
+      })
 
       app.get('/sponsors', async(req, res)=>{
         const query = {};
         const result = await sponsorCollection.find(query).toArray();
         res.send(result)
       })
+
+      
 
 
 
