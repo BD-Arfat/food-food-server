a305b2381f0ee4dc6a1b045d8233f13497fd3d64
BD-Arfat <arfat1islam5162@gmail.com>
2023-07-01 08:44:54 +0600

    add

--- index.js
+++ index.js
@@ -19,17 +19,44 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
 async function run() {
   try {
-    
-  } finally {}
+    const productCollection = client.db('food').collection('products');
+    const dronkCollection = client.db('food').collection('dronk-product');
+
+
+    app.get('/product', async (req, res) => {
+        const query = {};
+        const cursor = productCollection.find(query).limit(6);
+        const products = await cursor.toArray();
+        res.send(products)
+      });
+      app.get('/products', async (req, res) => {
+        const query = {};
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products)
+      });
+
+
+      app.get('/drink', async(req, res)=>{
+        const query = {};
+        const result = await dronkCollection.find(query).limit(6).toArray();
+        res.send(result)
+      })
+
+
+
+  } finally {
+
+  }
 }
 run().catch(console.dir);
 
